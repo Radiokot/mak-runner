6c1115223078696a8c5b53594dd7d3e408f0f17d
Oleg Koretsky <test.radiokot@gmail.com>
2020-01-15 19:08:07 +0200

    Update Mak asset code

--- src/main/kotlin/com/distributedlab/mak/Config.kt
+++ src/main/kotlin/com/distributedlab/mak/Config.kt
@@ -4,7 +4,7 @@ import java.util.function.Predicate
 
 object Config {
     const val TOKEND_API_URL = "https://api.staging.conto.me"
-    const val ASSET_CODE = "FFB406"
+    const val ASSET_CODE = "6DEB5D"
     const val ASSET_RECEIVER_EMAIL = "ole@mail.com"
     const val HTTP_LOGS_ENABLED = false
 
